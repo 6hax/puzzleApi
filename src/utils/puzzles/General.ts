a1ee486ed20a69ff0b216ac7f6dafde86876eeb7
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:43:01 -0300

    feat: add general knowledge puzzles

---
+++ src/utils/puzzles/General.ts
@@ -0,0 +1,24 @@
+const General = [
+    { id: 1, question: "O que é, o que é? Cai em pé e corre deitado?", answer: ["CHUVA", "chuva", "Chuva"] },
+    { id: 2, question: "O que é, o que é? Tem dentes, mas não morde?", answer: ["PENTE", "pente", "Pente"] },
+    { id: 3, question: "O que é, o que é? Quanto mais se tira, maior fica?", answer: ["BURACO", "buraco", "Buraco"] },
+    { id: 4, question: "O que é, o que é? Quanto mais se usa, menor fica?", answer: ["SABONETE", "sabonete", "Sabonete"] },
+    { id: 5, question: "O que é, o que é? Tem olhos, mas não vê?", answer: ["CEGO", "cego", "Cego"] },
+    { id: 6, question: "O que é, o que é? Enche uma casa, mas não ocupa espaço?", answer: ["LUZ", "luz", "Luz"] },
+    { id: 7, question: "O que é, o que é? Tem asa, mas não voa?", answer: ["AVIAO", "avião", "Avião"] },
+    { id: 8, question: "O que é, o que é? Anda com os pés na cabeça?", answer: ["PIOLHO", "piolho", "Piolho"] },
+    { id: 9, question: "O que é, o que é? Quanto mais quente, mais fresco?", answer: ["PAO", "pão", "Pão"] },
+    { id: 10, question: "O que é, o que é? Não se come, mas é bom para comer?", answer: ["TALHER", "talher", "Talher"] },
+    { id: 11, question: "O que é, o que é? Fica no meio do ovo?", answer: ["LETRA V", "letra v", "Letra V"] },
+    { id: 12, question: "O que é, o que é? Tem rabo, mas não é animal?", answer: ["COMETA", "cometa", "Cometa"] },
+    { id: 13, question: "O que é, o que é? Tem chifre, mas não é boi?", answer: ["CARACOL", "caracol", "Caracol"] },
+    { id: 14, question: "O que é, o que é? Tem um monte de irmãos gêmeos, mas todos são diferentes?", answer: ["IMPRESSAO DIGITAL", "impressão digital", "Impressão digital"] },
+    { id: 15, question: "O que é, o que é? Anda devagar, mas nunca para?", answer: ["RELOGIO", "relógio", "Relógio"] },
+    { id: 16, question: "O que é, o que é? Quanto mais se tem, menos se vê?", answer: ["ESCURO", "escuro", "Escuro"] },
+    { id: 17, question: "O que é, o que é? Tem cidade mas não tem casa, tem rio mas não tem água?", answer: ["MAPA", "mapa", "Mapa"] },
+    { id: 18, question: "O que é, o que é? Pesa mais quando está vazio?", answer: ["BALAO", "balão", "Balão"] },
+    { id: 19, question: "O que é, o que é? Tem cabeça e tem dente, mas não tem boca?", answer: ["ALHO", "alho", "Alho"] },
+    { id: 20, question: "O que é, o que é? Quanto mais se perde, mais se tem?", answer: ["SONO", "sono", "Sono"] }
+  ]
+
+  export default General
