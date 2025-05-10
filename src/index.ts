18bd28ca7a750cfa4f36dd1ebe21d40114101850
haxbash <bluemaquinajp@gmail.com>
2025-05-10 05:17:19 -0300

    feat: add support to cors

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,47 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 import { 
   getCategories, 
   getRandomQuestionFromCategory,
   getRandomQuestionFromAnyCategory
-} from "./utils/functions/puzzleUtils.js"
-import "dotenv/config"
+} from "./utils/functions/puzzleUtils.js";
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from 'url';
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(cors());
+
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.get('/categories', async (_, res) => {
-  const allCategories = await getCategories()
-  res.status(200).json(allCategories)
-})
+  const allCategories = await getCategories();
+  res.status(200).json(allCategories);
+});
 
 app.get('/randomPuzzle', async (_, res) => {
-  const puzzle = await getRandomQuestionFromAnyCategory()
-  res.status(200).json(puzzle)
-})
+  const puzzle = await getRandomQuestionFromAnyCategory();
+  res.status(200).json(puzzle);
+});
 
 app.get('/randomPuzzleCategory/:category', async (req, res) => {
-  const { category } = req.params
-  const puzzle = await getRandomQuestionFromCategory(category)
-  res.status(200).json(puzzle)
-})
+  const { category } = req.params;
+  const puzzle = await getRandomQuestionFromCategory(category);
+  res.status(200).json(puzzle);
+});
 
-app.get('/', (_, res) => res.status(200).send('puzzeApli'))
 
-app.listen(PORT, () => console.log(`🟢 Server running on port ${PORT}`))
+app.get('*', (_, res) => {
+  res.redirect('/');
+});
+
+app.listen(PORT, () => console.log(`🟢 Server rodando na porta ${PORT}`));
 
