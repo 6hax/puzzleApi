a0fe50d1ff0b2e251967cc2821790576a7175902
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:48:46 -0300

    feat: add historical facts questions

---
+++ src/utils/puzzles/history.ts
@@ -0,0 +1,14 @@
+const history = [
+    { id: 1, question: "Em que ano o Brasil foi descoberto?", answer: ["1500", "1500"] },
+    { id: 2, question: "Quem foi o primeiro presidente do Brasil?", answer: ["DEODORO DA FONSECA", "Deodoro da Fonseca", "deodoro da fonseca"] },
+    { id: 3, question: "Qual foi o último imperador do Brasil?", answer: ["D. PEDRO II", "D. Pedro II", "d. pedro ii"] },
+    { id: 4, question: "Em que ano terminou a Segunda Guerra Mundial?", answer: ["1945", "1945"] },
+    { id: 5, question: "Quem descobriu a América?", answer: ["CRISTOVAO COLOMBO", "Cristóvão Colombo", "cristovao colombo"] },
+    { id: 6, question: "Qual foi a primeira capital do Brasil?", answer: ["SALVADOR", "Salvador", "salvador"] },
+    { id: 7, question: "Quem foi o líder da Revolução Russa?", answer: ["LENIN", "Lenin", "lenin"] },
+    { id: 8, question: "Qual civilização construiu as pirâmides?", answer: ["EGIPCIA", "Egípcia", "egípcia"] },
+    { id: 9, question: "Em que ano a escravidão foi abolida no Brasil?", answer: ["1888", "1888"] },
+    { id: 10, question: "Quem foi o primeiro homem a pisar na Lua?", answer: ["NEIL ARMSTRONG", "Neil Armstrong", "neil armstrong"] }
+  ]
+
+  export default history
