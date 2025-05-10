0e730842d596dc2ce0f021dbbbd788b528d678c4
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:53:39 -0300

    feat: add culinary questions

---
+++ src/utils/puzzles/food.ts
@@ -0,0 +1,14 @@
+const food = [
+    { id: 1, question: "Qual é o prato mais famoso do Brasil?", answer: ["FEIJOADA", "Feijoada", "feijoada"] },
+    { id: 2, question: "Qual é o ingrediente principal do sushi?", answer: ["ARROZ", "Arroz", "arroz"] },
+    { id: 3, question: "Qual é a fruta mais consumida no mundo?", answer: ["TOMATE", "Tomate", "tomate"] },
+    { id: 4, question: "Qual é o país de origem da pizza?", answer: ["ITALIA", "Itália", "italia"] },
+    { id: 5, question: "Qual é o nome do doce feito de leite condensado e chocolate?", answer: ["BRIGADEIRO", "Brigadeiro", "brigadeiro"] },
+    { id: 6, question: "Qual é o café da manhã típico brasileiro?", answer: ["PAO COM MANTEIGA", "Pão com manteiga", "pão com manteiga"] },
+    { id: 7, question: "Qual é o nome do prato feito com carne seca e farinha?", answer: ["FEIJAO TROPEIRO", "Feijão Tropeiro", "feijão tropeiro"] },
+    { id: 8, question: "Qual é o país que mais consome arroz no mundo?", answer: ["CHINA", "China", "china"] },
+    { id: 9, question: "Qual é o nome do prato feito com peixe cru marinado?", answer: ["CEVICHE", "Ceviche", "ceviche"] },
+    { id: 10, question: "Qual é o ingrediente principal do pão?", answer: ["FARINHA", "Farinha", "farinha"] }
+  ]
+
+  export default food
