8e6a707ab48c02968a47993e5706091affa295e8
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:56:36 -0300

    refactor: consolidate all puzzle categories

---
+++ src/utils/puzzles/index.ts
@@ -0,0 +1,29 @@
+import general from './general';
+import mathematics from './mathematics';
+import technology from './technology';
+import movies from './movies';
+import literature from './literature';
+import geography from './geography';
+import history from './history';
+import science from './science';
+import music from './music';
+import animals from './animals';
+import food from './food';
+import art from './art';
+
+const puzzles = [
+general,
+mathematics,
+technology,
+movies,
+literature,
+geography,
+history,
+science,
+music,
+animals,
+food,
+art
+]
+
+export default puzzles
