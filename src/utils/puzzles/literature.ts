be41bda8aea47dc081a98b377476366c9094fe39
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:47:11 -0300

    feat: add literature questions

---
+++ src/utils/puzzles/literature.ts
@@ -0,0 +1,14 @@
+const literature = [
+    { id: 1, question: "Quem escreveu 'Dom Casmurro'?", answer: ["MACHADO DE ASSIS", "Machado de Assis", "machado de assis"] },
+    { id: 2, question: "Qual é o nome do autor de '1984'?", answer: ["GEORGE ORWELL", "George Orwell", "george orwell"] },
+    { id: 3, question: "Qual livro brasileiro tem a personagem 'Capitu'?", answer: ["DOM CASMURRO", "Dom Casmurro", "dom casmurro"] },
+    { id: 4, question: "Qual é o nome do poeta que escreveu 'Os Lusíadas'?", answer: ["LUIS DE CAMOES", "Luís de Camões", "luis de camoes"] },
+    { id: 5, question: "Qual escritor criou 'Sherlock Holmes'?", answer: ["ARTHUR CONAN DOYLE", "Arthur Conan Doyle", "arthur conan doyle"] },
+    { id: 6, question: "Qual é o nome da autora de 'Harry Potter'?", answer: ["J.K. ROWLING", "J.K. Rowling", "j.k. rowling"] },
+    { id: 7, question: "Qual livro começa com 'Era uma vez...'?", answer: ["MUITOS LIVROS", "Muitos livros", "muitos livros"] },
+    { id: 8, question: "Qual é o nome do autor de 'O Pequeno Príncipe'?", answer: ["ANTOINE DE SAINT-EXUPERY", "Antoine de Saint-Exupéry", "antoine de saint-exupery"] },
+    { id: 9, question: "Qual é o livro mais vendido do mundo depois da Bíblia?", answer: ["DOM QUIXOTE", "Dom Quixote", "dom quixote"] },
+    { id: 10, question: "Qual escritor brasileiro escreveu 'Vidas Secas'?", answer: ["GRACILIANO RAMOS", "Graciliano Ramos", "graciliano ramos"] }
+  ]
+
+  export default literature
