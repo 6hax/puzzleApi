ae28d79f0567cb3f54001d6987246fa3dbca91db
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:52:57 -0300

    feat: create animal trivia set

---
+++ src/utils/puzzles/animals.ts
@@ -0,0 +1,14 @@
+const animals = [
+    { id: 1, question: "Qual é o maior animal terrestre?", answer: ["ELEFANTE", "Elefante", "elefante"] },
+    { id: 2, question: "Qual é o único mamífero que voa?", answer: ["MORCEGO", "Morcego", "morcego"] },
+    { id: 3, question: "Qual é o animal mais rápido do mundo?", answer: ["GUEPARDO", "Guepardo", "guepardo"] },
+    { id: 4, question: "Qual é o maior peixe do mundo?", answer: ["TUBARAO-BALEIA", "Tubarão-baleia", "tubarão-baleia"] },
+    { id: 5, question: "Qual animal é conhecido como 'rei da selva'?", answer: ["LEAO", "Leão", "leão"] },
+    { id: 6, question: "Qual é o único animal que consegue se reconhecer no espelho?", answer: ["GOLFINHO", "Golfinho", "golfinho"] },
+    { id: 7, question: "Qual é o animal nacional do Brasil?", answer: ["SABIA", "Sabiá", "sabiá"] },
+    { id: 8, question: "Qual é o único mamífero venenoso?", answer: ["ORNITORRINCO", "Ornitorrinco", "ornitorrinco"] },
+    { id: 9, question: "Qual é o animal mais barulhento do mundo?", answer: ["BALEIA AZUL", "Baleia Azul", "baleia azul"] },
+    { id: 10, question: "Qual é o animal que dorme de pé?", answer: ["CAVALO", "Cavalo", "cavalo"] }
+  ]
+
+  export default animals
