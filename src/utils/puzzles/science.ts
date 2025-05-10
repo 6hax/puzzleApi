398a61ba24c1a4c98be41443769d10e508e2825a
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:49:57 -0300

    [

---
+++ src/utils/puzzles/science.ts
@@ -0,0 +1,13 @@
+const science = [
+    { id: 1, question: "Qual é o elemento químico com símbolo 'O'?", answer: ["OXIGENIO", "Oxigênio", "oxigênio"] },
+    { id: 2, question: "Quantos planetas tem o sistema solar?", answer: ["8", "8"] },
+    { id: 3, question: "Qual é o maior órgão do corpo humano?", answer: ["PELE", "Pele", "pele"] },
+    { id: 4, question: "Qual é a fórmula química da água?", answer: ["H2O", "H2O"] },
+    { id: 5, question: "Quem formulou a teoria da relatividade?", answer: ["EINSTEIN", "Einstein", "einstein"] },
+    { id: 6, question: "Qual é o gás mais abundante na atmosfera?", answer: ["NITROGENIO", "Nitrogênio", "nitrogênio"] },
+    { id: 7, question: "Qual é o processo de respiração das plantas?", answer: ["FOTOSSINTESE", "Fotossíntese", "fotossíntese"] },
+    { id: 8, question: "Qual é o nome do cientista que descobriu a penicilina?", answer: ["ALEXANDER FLEMING", "Alexander Fleming", "alexander fleming"] },
+    { id: 9, question: "Qual é o metal líquido à temperatura ambiente?", answer: ["MERCURIO", "Mercúrio", "mercúrio"] },
+    { id: 10, question: "Qual é o nome do processo de divisão celular?", answer: ["MITOSE", "Mitose", "mitose"] }
+  ]
+  export default science
