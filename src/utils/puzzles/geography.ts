0dad6c0e622d29e4c888f8db16c669f7c9f6c37b
haxbash <bluemaquinajp@gmail.com>
2025-05-10 03:48:12 -0300

    feat: introduce geography puzzles

---
+++ src/utils/puzzles/geography.ts
@@ -0,0 +1,13 @@
+const geography = [
+    { id: 1, question: "Qual é o maior país do mundo em área territorial?", answer: ["RUSSIA", "Rússia", "russia"] },
+    { id: 2, question: "Qual é o rio mais longo do mundo?", answer: ["NILO", "Nilo", "nilo"] },
+    { id: 3, question: "Qual é a capital do Canadá?", answer: ["OTTAWA", "Ottawa", "ottawa"] },
+    { id: 4, question: "Qual país tem formato de bota?", answer: ["ITALIA", "Itália", "italia"] },
+    { id: 5, question: "Qual é o menor país do mundo?", answer: ["VATICANO", "Vaticano", "vaticano"] },
+    { id: 6, question: "Qual é o oceano mais extenso?", answer: ["PACIFICO", "Pacífico", "pacífico"] },
+    { id: 7, question: "Qual é a montanha mais alta do mundo?", answer: ["EVEREST", "Everest", "everest"] },
+    { id: 8, question: "Qual é o deserto mais quente do mundo?", answer: ["SAARA", "Saara", "saara"] },
+    { id: 9, question: "Quantos estados tem o Brasil?", answer: ["26", "26"] },
+    { id: 10, question: "Qual é a capital da Austrália?", answer: ["CAMBERRA", "Camberra", "camberra"] }
+  ]
+  export default geography
